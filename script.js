dd087c5febec1c2273a3c8c9c9f40b14f3f1c4b3
Shahid <109452231+raza-shahi@users.noreply.github.com>
2026-01-23 00:38:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+const myDiv = document.getElementById("output");
+
+let arr = [1,2,3,4];
+
+let promise =(data)=>{
+	return new Promise((resolve,reject)=>{
+	setTimeout(()=>{
+		resolve(data.filter(item=> item%2==0));
+	},1000)
+ });
+}
+let promise2 = (data)=>{
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			resolve(data.map(item = item*2))
+		},3000)
+	})
+}
+
+promise(arr).then((data)=>{
+	myDiv.innerHTML = data;
+	return promise2(data);
+}).then((data)=>{
+	myDiv.innerHTML = data;
+}).cathc((e)=>{
+	console.log(e);
+})
+
+
+
+
